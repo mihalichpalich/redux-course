363379c394add7d05ac65a2038030e0c7fcf34ee
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-02 15:36:08 +0300

    освоил триггер событий

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 class App extends Component {
+  addTrack() {
+    console.log('addTrack', this.trackInput.value);
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value = "";
+  }
+
   render() {
     console.log(this.props.testStore);
 
     return (
       <div>
-        <input type="text" />
-        <button>Add track</button>
+        <input type="text" ref={(input) => {this.trackInput = input}}/> {/*ref используется для сохранения ссылки на input*/}
+        <button onClick={this.addTrack.bind(this)}>Add track</button>
         <ul>
           {this.props.testStore.map((track, index) =>
             <li key={index}>{track}</li>
@@ -21,7 +27,11 @@ class App extends Component {
 
 export default connect( //получаем в качестве props для компонента <App /> данные из store
   state => ({
-    testStore: state
+    testStore: state //все методы функций записываются в this.props
   }),
-  dispatch => ({})
+  dispatch => ({
+    onAddTrack: (trackName) => {
+      dispatch({type: 'ADD_TRACK', payload: trackName});
+    }
+  })
 )(App);

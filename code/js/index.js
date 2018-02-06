97c51bd22edd5bc54c55e3505c33dadca3137948
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-06 20:20:13 +0300

    установил redux

--- code/js/index.js
+++ code/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h2>Здесь может быть любой текст</h2>, 
+  <h2>Example</h2>,
   document.getElementById('fieldToShow')
 );

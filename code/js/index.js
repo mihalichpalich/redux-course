b8800aac9304f98cad8a7c1915e5d597fe86691b
mihalichpalich <pavlovtfspb@gmail.com>
2018-02-07 23:35:44 +0300

    освоил подключение provider

--- code/js/index.js
+++ code/js/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import allReducers from './reducers';
+import WebPage from './components/WebPage';
 
 const store = createStore(allReducers); //в стор можно передавать только 1 объект
 
 ReactDOM.render(
-  <h2>Example</h2>,
+  <Provider store={store}>
+    <WebPage />
+  </Provider>,
   document.getElementById('fieldToShow')
 );
